75749e182bd8ebdf73e1aaeb308c39d1c55d3932
Bryan C. Geraghty <bryan@ravensight.org>
2011-05-24 01:20:39 -0500

    Create new authorization interfaces and implemented them in authorization module

---
+++ Authorization/OrganizationInterface.php
@@ -0,0 +1,33 @@
+<?php // vim:ts=3:sts=3:sw=3:et:
+
+/**
+ * Authorization organization model interface
+ *
+ * PHP Version 5
+ *
+ * @category  PHP
+ * @package   MindFrame2
+ * @author    Bryan C. Geraghty <bryan@ravensight.org>
+ * @copyright 2005-2011 Bryan C. Geraghty
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU LGPL
+ * @link      https://github.com/archwisp/MindFrame2
+ */
+
+/**
+ * Authorization organization model interface
+ *
+ * @category PHP
+ * @package  MindFrame2
+ * @author   Bryan C. Geraghty <bryan@ravensight.org>
+ * @license  http://www.gnu.org/licenses/lgpl-3.0.txt GNU LGPL
+ * @link     https://github.com/archwisp/MindFrame2
+ */
+interface MindFrame2_Authorization_OrganizationInterface
+{
+   /**
+    * Returns the identifier associated with the organization
+    *
+    * @reutn mixed
+    */
+   public function getOrganizationId();
+}
---
+++ Authorization/RoleInterface.php
@@ -0,0 +1,56 @@
+<?php // vim:ts=3:sts=3:sw=3:et:
+
+/**
+ * Authorization role model interface
+ *
+ * PHP Version 5
+ *
+ * @category  PHP
+ * @package   MindFrame2
+ * @author    Bryan C. Geraghty <bryan@ravensight.org>
+ * @copyright 2005-2011 Bryan C. Geraghty
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU LGPL
+ * @link      https://github.com/archwisp/MindFrame2
+ */
+
+/**
+ * Authorization role model interface
+ *
+ * @category PHP
+ * @package  MindFrame2
+ * @author   Bryan C. Geraghty <bryan@ravensight.org>
+ * @license  http://www.gnu.org/licenses/lgpl-3.0.txt GNU LGPL
+ * @link     https://github.com/archwisp/MindFrame2
+ */
+interface MindFrame2_Authorization_RoleInterface
+{
+   /**
+    * Adds the specified permission to the role
+    *
+    * @return void
+    */
+   public function addPermission(
+      MindFrame2_Authorization_PermissionInterface $permission);
+
+   /**
+    * Returns the organization to which the role is assigned
+    *
+    * @reutn MindFrame2_Authorization_OrganizationInterface
+    */
+   public function getOrganization();
+
+   /**
+    * Returns the array of permission which are assigned to the role
+    *
+    * @return array
+    */
+   public function getPermissions();
+
+   /**
+    * Assignes the role to the specified organization
+    *
+    * @return void
+    */
+   public function setOrganization(
+      MindFrame2_Authorization_OrganizationInterface $organization);
+}
